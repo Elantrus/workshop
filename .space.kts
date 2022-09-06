19be89f0f7c5b127b879250e409f5b067c3989c0
Lazaro Junior Silva <spynrt@gmail.com>
2022-09-06 01:47:41 +0000

    .NET6 Tests

--- .space.kts
+++ .space.kts
@@ -1,9 +1,9 @@
-/**
-* JetBrains Space Automation
-* This Kotlin-script file lets you automate build activities
-* For more info, see https://www.jetbrains.com/help/space/automation.html
-*/
-
-job("Tests") {
-    container("elantrus.registry.jetbrains.space/p/workshop.backend/workshop.api/dockerfile")
+job(".NET Core test"){
+    container(image = "mcr.microsoft.com/dotnet/sdk:6.0"){
+        shellScript {
+            content = """
+                dotnet test
+            """
+        }
+    }
 }
